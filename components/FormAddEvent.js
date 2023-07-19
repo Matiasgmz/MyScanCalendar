f88c8ebaaf9b233ec240e3acb1d2f7eb4acfba9b
Matias gomez <mrmatiasgomez@gmail.com>
2023-07-19 16:04:12 +0200

    Ajout de la date en français

--- components/FormAddEvent.js
+++ components/FormAddEvent.js
@@ -72,6 +72,7 @@ export default function FormAddEvent() {
                     mode={'date'}
                     is24Hour={true}
                     onChange={onChange}
+                    locale='fr'
                 />
 
                 <DateTimePicker
